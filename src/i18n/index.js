935ab934474a036fdb9fbe596bda9e21c4cd6f08
crosalot <crosalot@gmail.com>
2019-10-24 19:03:21 +0700

    forgot Untracked files

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,6 +3,7 @@ import i18n from 'i18n';
 
 const LANGUAGE_CODE = process.env.LANGUAGE_CODE || 'zh_TW';
 
+console.log(__dirname + '/locales')
 console.log('LANGUAGE_CODE', LANGUAGE_CODE)
 i18n.configure({
   locales:[LANGUAGE_CODE],
